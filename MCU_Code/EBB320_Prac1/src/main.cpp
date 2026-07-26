b1fa57704c15612a282f4cc41605e72033dd4d83
Nelis <u24704611@tuks.co.za>
2026-07-26 17:37:32 +0200

    added live switching of duty cycle for PWM signals.

--- MCU_Code/EBB320_Prac1/src/main.cpp
+++ MCU_Code/EBB320_Prac1/src/main.cpp
@@ -15,16 +15,29 @@ int PWM_Flag = 0; //By default only pwm1 is on.
 bool start = 0; //Start off.   
 
 int timer = 0;
-const int PWM1 = 0;
-const int PWM2 = 0;
-const int Meas1 = 0;
-const int Meas2 = 0;
+const int PWM1 = GPIO_NUM_32;
+const int PWM2 = GPIO_NUM_33;
+const int Meas1 = GPIO_NUM_34;
+const int Meas2 = GPIO_NUM_35;
+
+const double A = 3.3/4095.0;
+const int PWM_freq = 10;
+const int PWM_res = 8;
 
 const int BaudRate = 115200;
+const int sampling_period = 5;
 
 void setup() {
-  // Start the USB-C serial/UART port
+  //Start the USB-C serial/UART port.
   Serial.begin(BaudRate);
+
+  //Setup I/O pins.
+  pinMode(Meas1, INPUT);
+  pinMode(Meas2, INPUT);
+
+  ledcChangeFrequency(0, PWM_freq, PWM_res);
+  ledcAttachPin (PWM1, 0);
+  ledcAttachPin(PWM2, 1);
 }
 
 void loop() {
@@ -55,6 +68,7 @@ void loop() {
     
     std::getline(stream, temp, ',');
     duty = std::stod(temp);
+    duty = pow(2, PWM_res)*(duty/100.0);
     
     std::getline(stream, temp, ',');
     PWM_Flag = std::stod(temp);
@@ -69,11 +83,14 @@ void loop() {
   if(start)
   {
     int start_time = millis();
-    std::string sending = fmt::format("{0},{1},{2}", 100*sin(millis()/500.0), 100*cos(millis()/500.0), millis() - timer); 
+    double elapsed = sampling_period - (millis() - start_time);
+
+    std::string sending = fmt::format("{0},{1},{2},{3}", analogRead(Meas1)*A, analogRead(Meas2)*A, millis() - timer, static_cast<int>(elapsed <= 0)); 
     Serial.println(String(sending.c_str()));
 
-    double elapsed = 10 - (millis() - start_time);
-    elapsed >= 0 ? delay(elapsed) : delay(10);
+    ledcWrite(0, duty);
+
+    elapsed >= 0 ? delay(elapsed) : delay(sampling_period);
   }
   //Otherwise deactivate PWM outputs.
   else
