8612ab6db712e421acfc09e769763907e42f8f46
Hanre Coetzer <hanre.coetzer@icloud.com>
2026-09-22 15:03:26 +0200

    Added dead time

--- Matlab_Code/tfest/tfest_code.m
+++ Matlab_Code/tfest/tfest_code.m
@@ -1,87 +1,77 @@
-%Obtaining the transfer function
-
-%Package the input arrays of both transistors into one matrix (Needs to be
-%columns)
+% Obtaining the transfer function
+% Package the input arrays of both transistors into one matrix (Needs to be columns)
 PWM1StepV1Data = readmatrix('..\Data1\Step_PWM1_V0.csv');
 PWM1StepV1 = PWM1StepV1Data(7920:end,1:5);
-
 PWM2StepV0Data = readmatrix('..\Data1\Step_PWM2_V0.csv');
 PWM2StepV0 = PWM2StepV0Data(6400:end,1:5);
 
 PWM1TestDataFile = readmatrix('..\Data1\Step_PWM1_VtestData50.csv');
 PWM1TestData = PWM1TestDataFile(6230:end,1:5);
-
 PWM2TestDataFile = readmatrix('..\Data1\Step_PWM2_VtestData50.csv');
 PWM2TestData = PWM2TestDataFile(6160:end,1:5);
 
-%pwm1 data prep
-
-
+% pwm1 data prep
 V_PWM1 = [PWM1StepV1(:,3)];
 Temp_PWM1 = [PWM1StepV1(:,1)-22.28, PWM1StepV1(:,2)-21.24];
 time_step = 0.005;
 
-%pwm2 data prep
+% pwm2 data prep
 V_PWM2 = [PWM2StepV0(:,4)];
 Temp_PWM2 = [PWM2StepV0(:,1)-20.26, PWM2StepV0(:,2)-19.05];
 
-%pwm1 test data prep
+% pwm1 test data prep
 V_PWM1_Test = [PWM1TestData(:,3)];
 Temp_PWM1_test = [PWM1TestData(:,1)-21.2, PWM1TestData(:,2)-20.2];
 
-%pwm2 test data prep
+% pwm2 test data prep
 V_PWM2_Test = [PWM2TestData(:,4)];
 Temp_PWM2_test = [PWM2TestData(:,1)-21.1, PWM2TestData(:,2)-20.2] ;
 
-
-
-
-
-%use iddata to set up the data for tfest
-
+% use iddata to set up the data for tfest
 tfest_data1 = iddata(Temp_PWM1, V_PWM1, time_step);
 tfest_data2 = iddata(Temp_PWM2, V_PWM2, time_step);
-testData1 = iddata(Temp_PWM1_test,V_PWM1_Test,time_step);
-testData2 = iddata(Temp_PWM2_test,V_PWM2_Test,time_step);
+testData1 = iddata(Temp_PWM1_test, V_PWM1_Test, time_step);
+testData2 = iddata(Temp_PWM2_test, V_PWM2_Test, time_step);
 
+% estimate nr of poles and zeros
+n_poles = 1; 
+n_zeros = 0; 
 
+% Estimate the transfer functions independently (SISO) to avoid idss error
+% tfest_data(samples, output, input)
+sys11 = tfest(tfest_data1(:, 1, 1), n_poles, n_zeros, NaN); % PWM1 to T1
+sys21 = tfest(tfest_data1(:, 2, 1), n_poles, n_zeros, NaN); % PWM1 to T2
 
+sys12 = tfest(tfest_data2(:, 1, 1), n_poles, n_zeros, NaN); % PWM2 to T1
+sys22 = tfest(tfest_data2(:, 2, 1), n_poles, n_zeros, NaN); % PWM2 to T2
 
+% Combine the individual SISO models back into SIMO systems
+simo_transfer1 = [sys11; sys21];
+simo_transfer2 = [sys12; sys22];
 
-
-%estimate nr of poles and zeros, 2 poles since there are two transistors
-n_poles = [2; 2];
-n_zeors = [1;1];
-
-%estimate the transfer functions
-simo_transfer1 = tfest(tfest_data1,n_poles,n_zeors);
-simo_transfer2 = tfest(tfest_data2, n_poles, n_zeors);
-
-
-
-%set up graph details
+% set up graph details
 tfest_data1.Name = 'Measured Data';
 tfest_data2.Name = 'Measured Data';
 testData1.Name = 'Measured Data';
 testData2.Name = 'Measured Data';
-
 simo_transfer2.Name = 'Simulated Model';
 simo_transfer1.Name = 'Simulated Model';
-
-
 
 figure(1)
 compare(tfest_data1, simo_transfer1);
 legend('Location', 'best');
 ylabel('Change in Temperature (C)')
+
 figure(2)
 compare(tfest_data2, simo_transfer2);
 legend('Location', 'best');
 ylabel('Change in Temperature (C)')
+
 figure(3)
 compare(testData1, simo_transfer1);
 legend('Location', 'best');
 ylabel('Change in Temperature (C)')
+
 figure(4)
 compare(testData2, simo_transfer2);
 legend('Location', 'best');
@@ -90,35 +80,35 @@ ylabel('Change in Temperature (C)')
 lines = findobj(gcf, 'Type', 'Line');
 set(lines, 'LineWidth', 1.5);
 
-%display the transfer functions of PWM1
+% display the transfer functions of PWM1
 [num1, den1] = tfdata(simo_transfer1(1, 1), 'v');
-
-
+delay1 = simo_transfer1(1, 1).IODelay; % Extract dead time
 [num2, den2] = tfdata(simo_transfer1(2, 1), 'v');
-
+delay2 = simo_transfer1(2, 1).IODelay;
 
 disp('--- Transfer Function 1 (PWM1 to T1) ---');
 disp('Numerator:'); disp(num1);
 disp('Denominator:'); disp(den1);
+disp(['Estimated Dead Time (s): ', num2str(delay1)]);
 
 disp('--- Transfer Function 2 (PWM1 to T2) ---');
 disp('Numerator:'); disp(num2);
 disp('Denominator:'); disp(den2);
+disp(['Estimated Dead Time (s): ', num2str(delay2)]);
 
-
-
-%display transfer functions of PWM2
+% display transfer functions of PWM2
 [num12, den12] = tfdata(simo_transfer2(1, 1), 'v');
-
-
+delay12 = simo_transfer2(1, 1).IODelay;
 [num22, den22] = tfdata(simo_transfer2(2, 1), 'v');
-
+delay22 = simo_transfer2(2, 1).IODelay;
 
 disp('--- Transfer Function 1 (PWM2 to T1) ---');
 disp('Numerator:'); disp(num12);
 disp('Denominator:'); disp(den12);
+disp(['Estimated Dead Time (s): ', num2str(delay12)]);
 
 disp('--- Transfer Function 2 (PWM2 to T2) ---');
 disp('Numerator:'); disp(num22);
 disp('Denominator:'); disp(den22);
+disp(['Estimated Dead Time (s): ', num2str(delay22)]);
 
